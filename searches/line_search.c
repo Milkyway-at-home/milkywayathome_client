6c556cb7439c2f6b65e3bfc4223385541822d1b3
Travis Desell <travis.desell@gmail.com>
2009-02-09 23:34:43 +0000

    *** empty log message ***

--- searches/line_search.c
+++ searches/line_search.c
@@ -10,13 +10,12 @@
 
 const char *LS_STR[] = {"success", "nan fitness in loop 1", "nan fitness in loop 2", "nan fitness in loop 3", "step < tol in loop 1", "eval max reached in loop 1", "eval max reached in loop 2"};
 
-#define LOOP1_MAX 30
-#define LOOP2_MAX 10
-
+#define LOOP1_MAX	30
+#define LOOP2_MAX	10
+#define NQUAD		3
 /********
 	*	Stopping Conditions
  ********/
-int		nquad = 3;
 double		tol = 1e-6;
 
 double evaluate_step(double* point, double step, double* direction, int number_parameters, double* current_point) {
@@ -31,7 +30,7 @@ int line_search(double* point, double initial_fitness, double* direction, int nu
 	int i, jump, eval_count;
 	double f1, f2, f3, fs;
 	double d1, d2, d3, dstar;
-	double step, a, b;
+	double step, a, b, c, top, bottom;
 	double* current_point;
 
 	current_point = (double*)malloc(sizeof(double) * number_parameters);
@@ -69,7 +68,7 @@ int line_search(double* point, double initial_fitness, double* direction, int nu
 	d1 = 0.0;
 	d2 = 1.0;
 	d3 = 2.0;
-	// f3 = evaluate( point = (d3 * step * direction) );
+	// f3 = evaluate( point + (d3 * step * direction) );
 	f3 = evaluate_step(point, d3 * step, direction, number_parameters, current_point);
 	(*evaluations_done)++;
 	printf("\t\tloop 2, evaluations: %d, step: %.15lf, fitness: %.15lf\n", (*evaluations_done), d3 * step, f3);
@@ -94,12 +93,19 @@ int line_search(double* point, double initial_fitness, double* direction, int nu
 		*	Find minimum
 	 ********/
 	fs = 0.0;
-	for (i = 0; i < nquad; i++) {
-		a = (d1*d1)*(f2-f3) + (d2*d2)*(f3-f1) + (d3*d3)*(f1-f2);
-		b = d1*(f2-f3) + d2*(f3-f1) + d3*(f1-f2);
-		dstar = 0.5 * (a/b);
+	for (i = 0; i < NQUAD; i++) {
+		//	a = (d1*d1)*(f2-f3) + (d2*d2)*(f3-f1) + (d3*d3)*(f1-f2);
+		//	b = d1*(f2-f3) + d2*(f3-f1) + d3*(f1-f2);
+		//	dstar = 0.5 * (a/b);
+		a = d1;
+		b = d2;
+		c = d3;
+		top = (b-a)*(b-a)*(f2-f3) - (b-c)*(b-c)*(f2-f1); 
+		bottom = (b-a)*(f2-f3) - (b-c)*(f2-f1);
+		dstar = b - 0.5 * (top / bottom);
 
-		if (dstar < d2 + tol && dstar > d2 - tol) dstar = d2 + tol;
+		if (dstar < d2 + tol && dstar >= d2) dstar = d2 + tol;
+		else if (dstar > d2 - tol && dstar <= d2) dstar = d2 - tol;
 
 		// fs = evaluate(point + (dstar * step * direction));
 		fs = evaluate_step(point, dstar * step, direction, number_parameters, current_point);
@@ -107,8 +113,8 @@ int line_search(double* point, double initial_fitness, double* direction, int nu
 		printf("\t\tloop 3, evaluations: %d, step: %.15lf, fitness: %.15lf\n", (*evaluations_done), (dstar * step), fs);
 		if (isnan(fs)) return LS_LOOP3_NAN;
 
-		if (dstar > d2 + tol) {
-			if (fs > f2) {
+		if (dstar > d2 ) {
+			if (fs < f2) {
 				d3 = dstar;
 				f3 = fs;
 			} else {
@@ -118,7 +124,7 @@ int line_search(double* point, double initial_fitness, double* direction, int nu
 				f2 = fs;
 			}
 		} else {
-			if (fs > f2) {
+			if (fs < f2) {
 				d1 = dstar;
 				f1 = fs;
 			} else {
