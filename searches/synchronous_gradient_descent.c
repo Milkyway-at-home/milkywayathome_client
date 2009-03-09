b4f553923a3cdeb184597a2ceb8a6ecd87bc8525
Travis Desell <travis.desell@gmail.com>
2009-03-09 01:19:01 +0000

    *** empty log message ***

--- searches/synchronous_gradient_descent.c
+++ searches/synchronous_gradient_descent.c
@@ -59,7 +59,7 @@ void synchronous_conjugate_gradient_descent(int number_arguments, char **argumen
 	double *previous_gradient;
 	double *previous_direction;
         int i, j, evaluations, number_iterations, retval;
-	int reset;
+	int reset, reset_count;
         double point_fitness;
         double *new_point;
 	double bet, betdiv;
@@ -71,7 +71,8 @@ void synchronous_conjugate_gradient_descent(int number_arguments, char **argumen
 		return;
 	}
 
-	reset = get_int_arg("-gd_reset", number_arguments, arguments);
+	reset = 0;
+	reset_count = get_int_arg("-gd_reset", number_arguments, arguments);
 	if (reset < 0) {
 		printf("argument: '-gd_reset #' not specified, quitting.\n");
 		return;
@@ -100,7 +101,7 @@ void synchronous_conjugate_gradient_descent(int number_arguments, char **argumen
 			break;
 		}
 
-		if (i > 0 && (i % reset) != 0) {
+		if (i > 0 && reset == 0) {
 			// bet = g_pres' * (g_pres - g_prev) / (g_prev' * g_prev);
 			bet = 0;
 			betdiv = 0;
@@ -126,9 +127,10 @@ void synchronous_conjugate_gradient_descent(int number_arguments, char **argumen
 		print_double_array(stdout, "\tnew point:", number_parameters, new_point);
 		printf("\tline search took: %d evaluations for new fitness: %.15lf, with result: [%s]\n", evaluations, point_fitness, LS_STR[retval]);
 
-		if (retval != LS_SUCCESS) break;
-                if (evaluations < 0) break;
+		if ((retval != LS_SUCCESS || evaluations < 0) && reset == 0) break;
                 memcpy(point, new_point, sizeof(double) * number_parameters);
+		reset++;
+		if (reset == reset_count) reset = 0;
         }
         free(new_point);
 	free(gradient);
