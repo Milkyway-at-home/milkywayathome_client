5a02e928a2bc66da5f955fdfdf2efd756862fa70
Travis Desell <travis.desell@gmail.com>
2009-09-11 21:16:02 +0000

    *** empty log message ***

---
+++ bin/start_de_meta.sh
@@ -0,0 +1,6 @@
+./start_de_test.sh mw_stars/stars222F5 mw_parameters/parameter_222F5_3s.txt de_s222_3s_$1_$2p_$3r_21 $4 $2 $1
+./start_de_test.sh mw_stars/stars222F5 mw_parameters/parameter_222F5_3s.txt de_s222_3s_$1_$2p_$3r_22 $4 $2 $1
+./start_de_test.sh mw_stars/stars222F5 mw_parameters/parameter_222F5_3s.txt de_s222_3s_$1_$2p_$3r_23 $4 $2 $1
+./start_de_test.sh mw_stars/stars222F5 mw_parameters/parameter_222F5_3s.txt de_s222_3s_$1_$2p_$3r_24 $4 $2 $1
+./start_de_test.sh mw_stars/stars222F5 mw_parameters/parameter_222F5_3s.txt de_s222_3s_$1_$2p_$3r_25 $4 $2 $1
+
