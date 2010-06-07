04db263de71b0a8c77320bb37b303ad8063bf27a
Matt Arsenault <arsenm2@rpi.edu>
2010-06-07 11:08:46 -0400

    Actually include the header with the debug macro.

---
+++ include/milkyway_priv.h
@@ -0,0 +1,41 @@
+/* Copyright 2010 Matthew Arsenault, Travis Desell, Dave Przybylo,
+Nathan Cole, Boleslaw Szymanski, Heidi Newberg, Carlos Varela, Malik
+Magdon-Ismail and Rensselaer Polytechnic Institute.
+
+This file is part of Milkway@Home.
+
+Milkyway@Home is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Milkyway@Home is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Milkyway@Home.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _MILKYWAY_PRIV_H_
+#define _MILKYWAY_PRIV_H_
+
+#include <stdio.h>
+#include <assert.h>
+
+#ifndef DEBUG
+
+/* convenient functions for printing debugging stuffs */
+#define MW_DEBUG(msg, ...) printf("%s():%d: ", __func__, __LINE__);\
+                           printf(msg, __VA_ARGS__);
+#define MW_DEBUGMSG(msg) puts(msg)
+#else
+
+#define MW_DEBUG(msg, ...) ((void) 0)
+#define MW_DEBUGMSG(msg, ...) ((void) 0)
+
+#endif /* DEBUG */
+
+#endif /* _MILKYWAY_PRIV_H_ */
+
