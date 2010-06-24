73002df351ec0a45bcf832909fd386d22b787ed4
Matt Arsenault <arsenm2@rpi.edu>
2010-06-24 12:42:26 -0400

    nbody: Add large set of reference outputs.

---
+++ nbody/tests/test_set1/disk=miyamoto-nagai_halo=nfw_quad=true_criterion=exact.js
@@ -0,0 +1,58 @@
+{  //test results are using double
+"nbody-parameters-file": {
+    "nbody-context": {
+        "headline" : "disk=miyamoto-nagai_halo=nfw_quad=true_criterion=exact",
+        "criterion" : "exact",
+        "use-quadrupole-corrections" : true,
+        "accuracy-parameter" : 1.0,
+        "seed" : 0,
+
+        "potential" : {
+            "disk" : {
+                "miyamoto-nagai" : {
+                    "mass" : 4.45865888E5,
+                    "scale-length" : 6.5,
+                    "scale-height" : 0.26
+                }
+            },
+
+            "spherical" : {
+                "sphere" : {
+                    "mass" : 1.52954402E5,
+                    "r0-scale" : 0.7
+                }
+            },
+
+            "halo" : {
+                "nfw" : {
+                    "vhalo" : 73,
+                    "scale-length" : 12.0,
+                    "z-flattening" : 1.1,
+                    "x-flattening" : 0.8,
+                    "y-flattening" : 1.0,
+                    "triaxial-angle" : 0.1
+                }
+            }
+        },
+
+        "dwarf-model": {
+            "plummer" : {
+                "mass" : 10,
+                "nbody" : 100,
+                "scale-radius" : 0.3,
+                "time-orbit" : 4,
+                "time-dwarf" : 3.945
+            }
+        }
+    },
+
+    "initial-conditions": {
+        "useGalC" : false,
+        "angle-use-radians" : false,
+        "velocity" : [ -156, 79, 107 ],
+        "position" : [ 28.6, 218.0, 53.5 ]
+    },
+
+
+}}
+
---
+++ tools/GenerateTests.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+import Data.Char
+import Text.Printf
+import System.Directory
+import System.FilePath
+import System.Process
+
+data Criterion = NewCriterion | Exact | BH86 | SW93
+               deriving (Eq, Ord, Enum, Bounded)
+
+instance Show Criterion where
+  show NewCriterion = "New-Criterion"
+  show Exact        = "Exact"
+  show BH86         = "BH86"
+  show SW93         = "SW93"
+
+data Disk = Exponential | MiyamotoNagai
+          deriving (Eq, Ord, Enum, Bounded)
+
+instance Show Disk where
+  show Exponential   = "Exponential"
+  show MiyamotoNagai = "Miyamoto-Nagai"
+
+data Halo = Logarithmic | NFW | Triaxial
+          deriving (Eq, Show, Ord, Enum, Bounded)
+
+data NBodyPerm = NBodyPerm { useQuad   :: Bool,
+                             criterion :: Criterion,
+                             disk      :: Disk,
+                             halo      :: Halo
+                           }
+               deriving (Eq, Ord)
+
+instance Show NBodyPerm where
+  show (NBodyPerm q c d h) = printf "disk=%s_halo=%s_quad=%s_criterion=%s"
+                                     (lowshow d) (lowshow h) (lowshow q) (lowshow c)
+
+allTests = [NBodyPerm q c d h | q <- es, c <- es, d <- es, h <- es]
+  where es = [minBound .. maxBound]
+
+lower = map toLower
+lowshow = lower . show
+
+printTemplate :: String -> NBodyPerm -> String
+printTemplate ts np@(NBodyPerm q c d h) = printf ts (lowshow np) (lowshow c) (lowshow q) (lowshow d) (lowshow h)
+
+runTest dir bin name str = do
+  let fname prec = dir </> prec </> (name ++ "_results")
+  rawSystem bin [ "-s", str, "-o", fname "float"]
+  rawSystem bin [ "-s", str, "-o", fname "double", "-d"]
+
+makeRunTest dir bin ts np = do let name  = show np
+                                   str   = printTemplate ts np
+                                   testf = dir </> name <.> ".js"
+                               writeFile testf str
+                               runTest dir bin name str
+
+generateTests template testDir bin = do
+  createDirectoryIfMissing True (testDir </> "double")
+  createDirectoryIfMissing True (testDir </> "float")
+  ts <- readFile template
+  mapM_ (makeRunTest testDir bin ts) allTests
+
+
+main = generateTests "test_set1_template.js" "test_set1" "../bin/milkyway_nbody"
+
