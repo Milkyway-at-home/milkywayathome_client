82e84c9b871ab856b55f0e6bd6b60b3e0f121e50
Matt Arsenault <arsenm2@rpi.edu>
2010-06-26 11:55:18 -0400

    nbody: Fix some duplication

--- nbody/src/nbody_simulation.c
+++ nbody/src/nbody_simulation.c
@@ -8,15 +8,40 @@
 #include "nbody_priv.h"
 #include "nbody.h"
 
+inline static void gravmap(const NBodyCtx* ctx, NBodyState* st)
+{
+    bodyptr p;
+    const bodyptr endp = st->bodytab + ctx->model.nbody;
+    unsigned int nfcalc  = 0;         /* count force calculations */
+    unsigned int n2bcalc = 0;         /* count body-body interactions */
+    unsigned int nbccalc = 0;         /* count body-cell interactions */
+
+    maketree(ctx, st, st->bodytab, ctx->model.nbody);       /* build tree structure */
+
+    for (p = st->bodytab; p < endp; p++)
+    {
+        /* loop over all bodies */
+        hackgrav(ctx, st, p, Mass(p) > 0.0);     /* get force on each */
+        ++nfcalc;                                /* count force calcs */
+        n2bcalc += st->n2bterm;                  /* and 2-body terms */
+        nbccalc += st->nbcterm;                  /* and body-cell terms */
+    }
+
+}
+
 /* startrun: startup hierarchical N-body code. */
 inline static void startrun(const NBodyCtx* ctx, const InitialConditions* ic, NBodyState* st)
 {
     srand48(ctx->seed);              /* set random generator */
     generatePlummer(ctx, ic, st);    /* make test model */
 
-    st->nstep = 0;                   /* start counting steps */
-    st->tout  = st->tnow;            /* schedule first output */
+    st->tout       = st->tnow;            /* schedule first output */
     st->tree.rsize = ctx->tree_rsize;
+
+    /* CHECKME: Why does maketree get used twice for the first step? */
+    gravmap(ctx, st);               /* Take 1st step */
+    output(ctx, st);                /* do initial output */
+    st->nstep = 1;                  /* start counting steps */
 }
 
 /* stepsystem: advance N-body system one time-step. */
@@ -24,30 +49,10 @@ inline static void stepsystem(const NBodyCtx* ctx, NBodyState* st)
 {
     bodyptr p;
     vector dvel, dpos;
-    int nfcalc;          /* count force calculations */
-    int n2bcalc;         /* count body-body interactions */
-    int nbccalc;         /* count body-cell interactions */
 
     const bodyptr bodytab = st->bodytab;
     const bodyptr endp    = bodytab + ctx->model.nbody;
     const real dt         = inv(ctx->freq);         /* set basic time-step */
-
-    if (st->nstep == 0)                 /* about to take 1st step? */
-    {
-        printf("Building tree...Starting Nbody simulation...\n");
-        maketree(ctx, st, bodytab, ctx->model.nbody);       /* build tree structure */
-        printf("Tree made\n");
-        nfcalc = n2bcalc = nbccalc = 0;     /* zero counters */
-        for (p = bodytab; p < endp; p++)
-        {
-            /* loop over all bodies */
-            hackgrav(ctx, st, p, Mass(p) > 0.0);     /* get force on each */
-            ++nfcalc;                       /* count force calcs */
-            n2bcalc += st->n2bterm;          /* and 2-body terms */
-            nbccalc += st->nbcterm;          /* and body-cell terms */
-        }
-        output(ctx, st);                /* do initial output */
-    }
 
     for (p = bodytab; p < endp; p++)    /* loop over all bodies */
     {
@@ -57,17 +62,9 @@ inline static void stepsystem(const NBodyCtx* ctx, NBodyState* st)
         INCADDV(Pos(p), dpos);          /* advance r by 1 step */
     }
 
-    maketree(ctx, st, bodytab, ctx->model.nbody);     /* build tree structure */
-    nfcalc = n2bcalc = nbccalc = 0;          /* zero counters */
-    for (p = st->bodytab; p < endp; p++)     /* loop over bodies */
-    {
-        hackgrav(ctx, st, p, Mass(p) > 0.0);   /* get force on each */
-        ++nfcalc;                     /* count force calcs */
-        n2bcalc += st->n2bterm;           /* and 2-body terms */
-        nbccalc += st->nbcterm;           /* and body-cell terms */
-    }
+    gravmap(ctx, st);
 
-    for (p = bodytab; p < endp; p++) /* loop over all bodies */
+    for (p = bodytab; p < endp; p++)          /* loop over all bodies */
     {
         MULVS(dvel, Acc(p), 0.5 * dt);        /* get velocity increment */
         INCADDV(Vel(p), dvel);                /* advance v by 1/2 step */
@@ -75,7 +72,7 @@ inline static void stepsystem(const NBodyCtx* ctx, NBodyState* st)
 
     st->nstep++;           /* count another time step */
     st->tnow += dt;        /* finally, advance time */
-    output(ctx, st);             /* do major or minor output */
+    output(ctx, st);       /* do major or minor output */
 }
 
 
