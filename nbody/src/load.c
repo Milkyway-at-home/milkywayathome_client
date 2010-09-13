8dde29a30a915260dd0acec6370726bc4995577d
Matt Arsenault <arsenm2@rpi.edu>
2010-09-13 12:41:26 -0400

    nbody: Fix license headers

--- nbody/src/load.c
+++ nbody/src/load.c
@@ -1,9 +1,23 @@
-/* ************************************************************************** */
-/* load.C: routines to create tree.  Public routines: makeTree(). */
-/* */
-/* Copyright (c) 1993 by Joshua E. Barnes, Honolulu, HI. */
-/* It's free because it's yours. */
-/* ************************************************************************** */
+/* Copyright (c) 1993, 2001 Joshua E. Barnes, Honolulu, HI.
+   Copyright 2010 Matthew Arsenault, Travis Desell, Boleslaw
+Szymanski, Heidi Newberg, Carlos Varela, Malik Magdon-Ismail and
+Rensselaer Polytechnic Institute.
+
+This file is part of Milkway@Home.
+
+Milkyway@Home is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Milkyway@Home is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Milkyway@Home.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef _WIN32
   #include <sys/stat.h>
