58a794ff379488f4526708df5ee8244d44ab689b
Matt Arsenault <arsenm2@rpi.edu>
2011-06-12 02:08:45 -0400

    separation: Fix 32 bit build

--- cmake_modules/CheckSSE2.cmake
+++ cmake_modules/CheckSSE2.cmake
@@ -1,19 +1,19 @@
 # Copyright 2010 Matthew Arsenault, Travis Desell, Dave Przybylo,
 # Nathan Cole, Boleslaw Szymanski, Heidi Newberg, Carlos Varela, Malik
 # Magdon-Ismail and Rensselaer Polytechnic Institute.
-
+#
 # This file is part of Milkway@Home.
-
+#
 # Milkyway@Home is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # Milkyway@Home is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with Milkyway@Home.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -24,11 +24,13 @@ include(CheckIncludeFiles)
 
 if(SYSTEM_IS_X86)
   if(NOT MSVC)
-    set(DISABLE_SSE2_FLAGS "-mfpmath=sse -msse -msse2")
-    set(DISABLE_SSE3_FLAGS "-mno-sse3")
-    set(NO_SSE2_FLAGS "-mno-sse -mno-sse2 -mfpmath=387")
+    set(SSE2_FLAGS "-mfpmath=sse -msse -msse2")
     set(SSE3_FLAGS "-msse3")
     set(SSE4_FLAGS "-msse4")
+
+    set(DISABLE_SSE2_FLAGS "-mno-sse2 -mno-sse3")
+    set(DISABLE_SSE3_FLAGS "-mno-sse3")
+    set(NO_SSE2_FLAGS "-mfpmath=387 -mno-sse ${DISABLE_SSE2_FLAGS}")
   else()
     set(SSE2_FLAGS "/arch:SSE2 /D__SSE2__=1")
     set(DISABLE_SSE3_FLAGS "")
