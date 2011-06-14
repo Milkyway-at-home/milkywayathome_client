72434438b8bfe66bab7186fee0e648a7380112c8
Matt Arsenault <arsenm2@rpi.edu>
2011-06-14 18:25:34 -0400

    all: Set /subsystem link flag for MSVC

--- cmake_modules/MSVCFlags.cmake
+++ cmake_modules/MSVCFlags.cmake
@@ -1,27 +1,34 @@
 # Copyright 2010 Matthew Arsenault, Travis Desell, Dave Przybylo,
 # Nathan Cole, Boleslaw Szymanski, Heidi Newberg, Carlos Varela, Malik
 # Magdon-Ismail and Rensselaer Polytechnic Institute.
-
+#
 # This file is part of Milkway@Home.
-
+#
 # Milkyway@Home is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # Milkyway@Home is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with Milkyway@Home.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+include(CPUNameTest)
 
 macro(set_msvc_extra_link_flags)
   if(MSVC)
     set(CMAKE_EXE_LINKER_FLAGS "/VERBOSE:LIB ${CMAKE_EXE_LINKER_FLAGS}")
+
+    # Minimum is 5.0 on x86 and (5.02 on x64, same as default)
+    if(SYSTEM_IS_32)
+      set(CMAKE_EXE_LINKER_FLAGS "/SUBSYSTEM:CONSOLE,5.0 ${CMAKE_EXE_LINKER_FLAGS}")
+    endif()
+
     if(MSVC_USE_STATIC_CRT)
       set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /NODEFAULTLIB:msvcrt.lib")
     endif()
