03920ce90c3928bcac3073e5bf2259740283d54b
Matt Arsenault <arsenm2@rpi.edu>
2012-05-17 19:46:48 -0400

    separation: Maybe fix integral sizes for tests

--- separation/tests/stripe_11.lua
+++ separation/tests/stripe_11.lua
@@ -39,15 +39,15 @@ area = {
    {
       r_min = 16.0,
       r_max = 23.0,
-      r_steps = 140,
+      r_steps = 1400,
 
       mu_min = 150,
       mu_max = 229,
-      mu_steps = 160,
+      mu_steps = 1600,
 
       nu_min = -1.25,
       nu_max = 1.25,
-      nu_steps = 64
+      nu_steps = 640
    }
 }
 
--- separation/tests/stripe_21.lua
+++ separation/tests/stripe_21.lua
@@ -39,15 +39,15 @@ area = {
    {
       r_min = 16.0,
       r_max = 22.5,
-      r_steps = 140,
+      r_steps = 1400,
 
       mu_min = 133,
       mu_max = 210,
-      mu_steps = 160,
+      mu_steps = 1600,
 
       nu_min = -1.25,
       nu_max = 1.25,
-      nu_steps = 64
+      nu_steps = 640
    }
 }
 
