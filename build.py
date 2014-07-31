3526cbec22d33c45bf8f0df79d36a5e4ccf2768c
Roland Judd <rolandjudd@gmail.com>
2014-07-30 23:04:12 -0400

    Don't use CMAKE_FIND_ROOT_PATH for programs, only libraries and includes

--- build.py
+++ build.py
@@ -31,7 +31,7 @@ if os == "linux":
     if arch == "64":
         execute(["cmake", ".", "-DCMAKE_FIND_ROOT_PATH=/srv/chroot/hardy_amd64", "-DCMAKE_C_FLAGS=-m64", "-DCMAKE_CXX_FLAGS=-m64"])
     if arch == "32":
-        execute(["cmake", ".", "-DBUILD_32=ON", "-DCMAKE_FIND_ROOT_PATH=/srv/chroot/hardy_i386", "-DCMAKE_C_FLAGS=-m32", "-DCMAKE_CXX_FLAGS=-m32", "-DCMAKE_AR=/usr/bin/ar"])
+        execute(["cmake", ".", "-DBUILD_32=ON", "-DCMAKE_FIND_ROOT_PATH=/srv/chroot/hardy_i386", "-DCMAKE_C_FLAGS=-m32", "-DCMAKE_CXX_FLAGS=-m32", "-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER"])
         
     execute(["make"])
 
