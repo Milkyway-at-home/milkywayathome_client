4678aa5398c43a5dfe077f60350fe31565e2f642
weissj3 <weissj3@rpi.edu>
2014-09-03 09:37:33 -0400

    Added Separation OpenCL/CPU Build Support

--- build.py
+++ build.py
@@ -24,13 +24,13 @@ if len(sys.argv) != 4:
 # Make sure os and architecture are valid
 os  = sys.argv[1]
 arch = sys.argv[2]
-nbody_openmp = sys.argv[3]
+nbody_openmp_sep_opencl = sys.argv[3]
 assert os in ["win", "linux", "mac"], "ERROR: Unknown OS " + os
 assert arch in ["32", "64"], "ERROR: Unknown arch " + bit 
-assert nbody_openmp in ["ON", "OFF"], "ERROR: Set NBODY_OPENMP to ON or OFF"
+assert nbody_openmp_sep_opencl in ["ON", "OFF"], "ERROR: Set NBODY_OPENMP to ON or OFF"
 
 # CMake flags used for all platforms
-cmake_shared_flags = ["-DBOINC_RELEASE_NAMES=ON", "-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER", "-DNBODY_OPENMP=" + nbody_openmp]
+cmake_shared_flags = ["-DBOINC_RELEASE_NAMES=ON", "-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER", "-DNBODY_OPENMP=" + nbody_openmp_sep_opencl, "-DSEPARATION_OPENCL=" + nbody_openmp_sep_opencl]
 
 # CMake flags used for windows
 cmake_windows_flags = ["-DNBODY_STATIC=ON"]
