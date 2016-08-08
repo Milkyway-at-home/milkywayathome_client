bea04f848e665f35d8375e5b5cfcb61d70462c5b
Sidd <shelts@rpi.edu>
2016-08-08 18:25:43 -0400

    added calculation of gamma function using lanczos approx

--- nbody/src/nbody_dwarf_potential.c
+++ nbody/src/nbody_dwarf_potential.c
@@ -71,10 +71,44 @@ static real gen_hern_pot(const Dwarf* model, real r)                            
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                             /* EINASTO */
 
-// static real einasto_den(real r, real A, real alpha)
-// {
-//     return mw_exp(-A * mw_pow(r, alpha));
-// }
+static real GammaFunc(const real z) 
+{
+    //Alogrithm for the calculation of the Lanczos Approx of the complete Gamma function 
+    //as implemented in Numerical Recipes 3rd ed, 2007.
+    real g = 4.7421875; //g parameter for the gamma function
+    real x, tmp, y, A_g;
+    
+    //these are the cn's
+    static const real coeff[14] = {57.1562356658629235,-59.5979603554754912,
+                                14.1360979747417471,-0.491913816097620199,.339946499848118887e-4,
+                                .465236289270485756e-4,-.983744753048795646e-4,.158088703224912494e-3,
+                                -.210264441724104883e-3,.217439618115212643e-3,-.164318106536763890e-3,
+                                .844182239838527433e-4,-.261908384015814087e-4,.368991826595316234e-5};
+    y = x = z;
+    tmp = x + g + 0.5;
+    tmp = (x + 0.5) * mw_log(tmp) - tmp;
+    A_g = 0.999999999999997092; //this is c0
+    
+    for (int j = 0; j < 14; j++) 
+    {
+        A_g += coeff[j] / ++y;
+    } //calculates the series approx sum
+        
+    //sqrt(2 * pi) = 2.5066282746310005
+    tmp += mw_log(2.5066282746310005 * A_g / x);//returns the log of the gamma function
+    
+    return mw_exp(tmp);
+}
+                            
+                            
+                            
+static real einasto_den(const Dwarf* model, real r)
+{
+    const real mass = model->mass;
+    const real h = model->h;
+    const real s = 
+    return mw_exp(-A * mw_pow(r, alpha));
+}
 // 
 // static real einasto_pot(real r, real A, real alpha)
 // {
