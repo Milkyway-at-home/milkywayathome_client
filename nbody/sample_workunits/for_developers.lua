e7ab3a3bd2e7724b6dab2cbf1a9d7023be405af7
shelts <shelts@rpi.edu>
2017-05-09 18:12:02 -0400

    new lua files for client side and for developers

--- nbody/sample_workunits/for_developers.lua
+++ nbody/sample_workunits/for_developers.lua
@@ -1,14 +1,23 @@
 -- /* Copyright (c) 2016 Siddhartha Shelton */
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
--- Client side Lua file
+-- DEAR LUA USER:
+-- This is the developer version of the lua parameter file. 
+-- It gives all the options you can have. 
+-- Many of these the client will not need.
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
-                
+        
+        
+        
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 totalBodies           = 20000   -- -- NUMBER OF BODIES           -- --
 nbodyLikelihoodMethod = "EMD"   -- -- HIST COMPARE METHOD        -- --
 nbodyMinVersion       = "1.64"  -- -- MINIMUM APP VERSION        -- --
 
+run_null_potential    = false   -- -- NULL POTENTIAL SWITCH      -- --
+two_component_model   = true    -- -- TWO COMPONENTS SWITCH      -- --
+print_reverse_orbit   = false   -- -- PRINT REVERSE ORBIT SWITCH -- --
+print_out_parameters  = true    -- -- PRINT OUT ALL PARAMETERS   -- --
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 -- -- -- -- -- -- -- -- -- DWARF STARTING LOCATION   -- -- -- -- -- -- -- --
 l  = 218
@@ -20,11 +29,16 @@ vz = 107
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
 function makePotential()
-    return  Potential.create{
-        spherical = Spherical.spherical{ mass  = 1.52954402e5, scale = 0.7 },
-        disk      = Disk.miyamotoNagai{ mass = 4.45865888e5, scaleLength = 6.5, scaleHeight = 0.26 },
-        halo      = Halo.logarithmic{ vhalo = 73, scaleLength = 12.0, flattenZ = 1.0 }
-    }
+   if(run_null_potential == true) then
+       print("running in null potential")
+       return nil
+   else
+        return  Potential.create{
+            spherical = Spherical.spherical{ mass  = 1.52954402e5, scale = 0.7 },
+            disk      = Disk.miyamotoNagai{ mass = 4.45865888e5, scaleLength = 6.5, scaleHeight = 0.26 },
+            halo      = Halo.logarithmic{ vhalo = 73, scaleLength = 12.0, flattenZ = 1.0 }
+        }
+   end
 end
 
 
@@ -70,29 +84,63 @@ function makeContext()
    }
 end
 
+-- soften_length = (mass_l * rscale_l + mass_d  * rscale_d) / (mass_d + mass_l)
+-- print('soften_length ', calculateEps2(totalBodies, soften_length ))
 
 function makeBodies(ctx, potential)
-    local firstModel
-    local finalPosition, finalVelocity
+  local firstModel
+  local finalPosition, finalVelocity
+    if(run_null_potential == true) then
+        print("placing dwarf at origin")
+        finalPosition, finalVelocity = Vector.create(0, 0, 0), Vector.create(0, 0, 0)
+    else 
+        finalPosition, finalVelocity = reverseOrbit{
+            potential = potential,
+            position  = lbrToCartesian(ctx, Vector.create(l, b, r)),
+            velocity  = Vector.create(vx, vy, vz),
+            tstop     = revOrbTime,
+            dt        = ctx.timestep / 10.0
+            }
+    end
     
-    finalPosition, finalVelocity = reverseOrbit{
-        potential = potential,
-        position  = lbrToCartesian(ctx, Vector.create(l, b, r)),
-        velocity  = Vector.create(vx, vy, vz),
-        tstop     = revOrbTime,
-        dt        = ctx.timestep / 10.0
-    }
+    if(print_reverse_orbit == true) then
+        local placeholderPos, placeholderVel = PrintReverseOrbit{
+            potential = potential,
+            position  = lbrToCartesian(ctx, Vector.create(l, b, r)),
+            velocity  = Vector.create(vx, vy, vz),
+            tstop     = .14,
+            tstopf    = .20,
+            dt        = ctx.timestep / 10.0
+        }
+        print('Printing reverse orbit')
+    end
     
-    firstModel = predefinedModels.mixeddwarf{
-        nbody       = totalBodies,
-        prng        = prng,
-        position    = finalPosition,
-        velocity    = finalVelocity,
-        comp1       = Dwarf.plummer{mass = mass_l, scaleLength = rscale_l}, -- Dwarf Options: plummer, nfw, general_hernquist
-        comp2       = Dwarf.plummer{mass = mass_d, scaleLength = rscale_d}, -- Dwarf Options: plummer, nfw, general_hernquist
-        ignore      = true
-    }
 
+  
+    if(two_component_model) then 
+        firstModel = predefinedModels.mixeddwarf{
+            nbody       = totalBodies,
+            prng        = prng,
+            position    = finalPosition,
+            velocity    = finalVelocity,
+            comp1       = Dwarf.plummer{mass = mass_l, scaleLength = rscale_l}, -- Dwarf Options: plummer, nfw, general_hernquist
+            comp2       = Dwarf.plummer{mass = mass_d, scaleLength = rscale_d}, -- Dwarf Options: plummer, nfw, general_hernquist
+            ignore      = true
+        }
+        
+    else
+        firstModel = predefinedModels.plummer{
+            nbody       = model1Bodies,
+            prng        = prng,
+            position    = finalPosition,
+            velocity    = finalVelocity,
+            mass        = dwarfMass,
+            scaleRadius = r0,
+            ignore      = false
+        }
+  
+    end
+  
   return firstModel
 end
 
@@ -142,3 +190,11 @@ dwarfMass = mass_l / light_mass_ratio
 rscale_t  = rscale_l / light_r_ratio
 rscale_d  = rscale_t *  (1.0 - light_r_ratio)
 mass_d    = dwarfMass * (1.0 - light_mass_ratio)
+
+if(print_out_parameters) then
+    print('forward time=', evolveTime, '\nrev time=',  revOrbTime)
+    print('mass_l sim=', mass_l, '\nmass_d sim=', mass_d)
+    print('light mass solar=', mass_l * 222288.47, '\ndark mass solar=', mass_d * 222288.47)
+    print('total mass solar= ', (mass_d + mass_l) * 222288.47)
+    print('rl = ', rscale_l, 'rd = ', rscale_d)
+end
