c8e67a13c21e5ef876000e453aed2d2cd7becabc
shelts <shelts@rpi.edu>
2017-10-04 12:59:23 -0400

    functional manually inputted body code '

--- nbody/src/nbody_manual_bodies.c
+++ nbody/src/nbody_manual_bodies.c
@@ -35,139 +35,126 @@ their copyright to their programs which execute similar algorithms.
 #include <stdio.h>
 
 /*      DWARF GENERATION        */
-static int nbGenerateManualBodiescore(lua_State* luaSt, unsigned int nbody, const char* body_file)
-{
-    /* generatePlummer: generate Plummer model initial conditions for test
-    * runs, scaled to units such that M = -4E = G = 1 (Henon, Heggie,
-    * etc).    See Aarseth, SJ, Henon, M, & Wielen, R (1974) Astr & Ap, 37,
-    * 183.
-    */
-        
-//         fscanf(body_inputs, "%f %f %f %f %f %f %f %f", xx, yy, zz, hh, rr, tt, ee, ww);
-//         mw_printf("%f\n", zz);
-        
-        unsigned int i;
-        int table;
-        Body b;
-        real r, v;
- 
-        real * x  = mwCalloc(nbody, sizeof(real));
-        real * y  = mwCalloc(nbody, sizeof(real));
-        real * z  = mwCalloc(nbody, sizeof(real));
-        real * vx = mwCalloc(nbody, sizeof(real));
-        real * vy = mwCalloc(nbody, sizeof(real));
-        real * vz = mwCalloc(nbody, sizeof(real));
-        real * masses = mwCalloc(nbody, sizeof(real));
-        
-        FILE* body_inputs;
-        body_inputs = fopen(body_file, "r");
-//         real tty, xx, yy, zz, vxx, vyy, vzz, mm;
-        char buf1[100];
-        char buf2[100];
-        char buf3[100];
-        char buf4[100];
-        char buf5[100];
-        char buf6[100];
-        char buf7[100];
-        char buf8[100];
-//         double buf1;
-//         double buf2;
-//         double buf3;
-//         double buf4;
-//         double buf5;
-//         double buf6;
-//         double buf7;
-//         double buf8;
-        
-//         char buf1;
-//         char buf2;
-//         char buf3;
-//         char buf4;
-//         char buf5;
-//         char buf6;
-//         char buf7;
-//         char buf8;
-        unsigned int counter = 0;
-        while (fscanf(body_inputs,"%s %s %s %s %s %s %s %s ",buf1, buf2, buf3, buf4, buf5, buf6, buf7, buf8)==8)
-//         while (fscanf(body_inputs,"%f %f %f %f %f %f %f %f ",buf1, buf2, buf3, buf4, buf5, buf6, buf7, buf8)==8)
-        {
-//             x[counter]      = strtod(buf2, NULL);
-//             y[counter]      = strtod(buf3, NULL);
-//             z[counter]      = strtod(buf4, NULL);
-//             vx[counter]     = strtod(buf5, NULL);
-//             vy[counter]     = strtod(buf6, NULL);
-//             vz[counter]     = strtod(buf7, NULL);
-//             masses[counter] = strtod(buf8, NULL);
-            
-            x[counter]      = atof(buf2);
-            y[counter]      = atof(buf3);
-            z[counter]      = atof(buf4);
-            vx[counter]     = atof(buf5);
-            vy[counter]     = atof(buf6);
-            vz[counter]     = atof(buf7);
-            masses[counter] = atof(buf8);
-            mw_printf("%s %s %s %s %s %s %s %s\n", buf1, buf2, buf3, buf4, buf5, buf6, buf7, buf8);
-//             mw_printf("%f %f %f %f %f %f %f %f\n", buf1, buf2, buf3, buf4, buf5, buf6, buf7, buf8);
-            counter++;
-//             mw_printf("%f %f %f %f %f %f %f\n", x[counter], y[counter], z[counter], vx[counter], vy[counter], vz[counter], masses[counter]);
-        }
-        
-        fclose(body_inputs);
-        mwvector vec;
-        mwbool isdark = TRUE;
-        mwbool islight = FALSE;
-       
-        
-     /*initializing particles:*/
-        memset(&b, 0, sizeof(b));
-        lua_createtable(luaSt, nbody, 0);
-        table = lua_gettop(luaSt);      
-//         int counter = 0;
-        
+static int nbGenerateManualBodiescore(lua_State* luaSt, const char* body_file)
+{   
+    
+    /*initializing particles:*/
+    int table;
+    Body b;
 
-        /*getting the radii and velocities for the bodies*/
+    FILE* body_inputs;
+    unsigned int lineNum = 0;
+    unsigned int fileCount = 0;
+    char lineBuf[1024];
+    int rc = 0;
+    real type;
+    mwbool error = FALSE;
+    body_inputs = mwOpenResolved(body_file, "r");
+    size_t fsize = mwCountLinesInFile(body_inputs);//get the number of lines in the file
+    
+    unsigned int nbody = fsize;
+    
+    if (body_inputs == NULL)//make sure the file is available
+    {
+        mw_printf("Error opening data file '%s'\n", body_file);
+        return NULL;
+    }
+    
+    real * x  = mwCalloc(fsize, sizeof(real));
+    real * y  = mwCalloc(fsize, sizeof(real));
+    real * z  = mwCalloc(fsize, sizeof(real));
+    real * vx = mwCalloc(fsize, sizeof(real));
+    real * vy = mwCalloc(fsize, sizeof(real));
+    real * vz = mwCalloc(fsize, sizeof(real));
+    real * masses = mwCalloc(fsize, sizeof(real));
+    
+    memset(&b, 0, sizeof(b));
+    lua_createtable(luaSt, nbody, 0);
+    table = lua_gettop(luaSt);      
+    
+    if (fsize == 0)//if the file is empty then throw error
+    {
+        mw_printf("Histogram line count = 0\n");
+        return NULL;
+    }
 
-        /* pushing the bodies */
-        for (i = 0; i < nbody; i++)
+    int counter = 0;
+    while (fgets(lineBuf, (int) sizeof(lineBuf), body_inputs))
+    {
+        ++lineNum;
+
+        if (strlen(lineBuf) + 1 >= sizeof(lineBuf))//check if we can store the data from that line
         {
-            b.bodynode.type = BODY(islight);
-            b.bodynode.mass = masses[i];
-            /*this actually gets the position and velocity vectors and pushes table of bodies*/
-            /*They are meant to give the dwarf an initial position and vel*/
-            /* you have to work for your bodynode */
-            b.bodynode.pos.x = x[i];
-            b.bodynode.pos.y = y[i];
-            b.bodynode.pos.z = z[i];
-            
-            b.vel.x = vx[i];
-            b.vel.y = vy[i];
-            b.vel.z = vz[i];
-            
-            assert(nbPositionValid(b.bodynode.pos));
-            pushBody(luaSt, &b);
-            lua_rawseti(luaSt, table, i + 1);
+            mw_printf("Error reading data file line %d (Line buffer too small): %s", lineNum, lineBuf);
+            error = TRUE;
+            break;
         }
+
+        /* Skip comments and blank lines */
+        if (lineBuf[0] == '#' || lineBuf[0] == '\n')
+            continue;
+
+        rc = sscanf(lineBuf,
+                    "%lf %lf %lf %lf %lf %lf %lf %lf \n",
+                    &type,
+                    &x[counter],
+                    &y[counter],
+                    &z[counter],
+                    &vx[counter],
+                    &vy[counter],
+                    &vz[counter],
+                    &masses[counter]
+                   );
         
-        /* go now and be free!*/
-        free(x);
-        free(y);
-        free(z);
-        free(vx);
-        free(vy);
-        free(vz);
-        free(masses);
+        counter++;
+        ++fileCount;
+    }
+    
+    fclose(body_inputs);
+
+    /* pushing the bodies */
+    for (int i = 0; i < nbody; i++)
+    {
+        b.bodynode.type = BODY(TRUE);
+        b.bodynode.mass = masses[i];
+
+        /*this actually gets the position and velocity vectors and pushes table of bodies*/
+        /*They are meant to give the dwarf an initial position and vel*/
+        /* you have to work for your bodynode */
+        b.bodynode.pos.x = x[i];
+        b.bodynode.pos.y = y[i];
+        b.bodynode.pos.z = z[i];
         
-        return 1;             
+        b.vel.x = vx[i];
+        b.vel.y = vy[i];
+        b.vel.z = vz[i];
+        
+    mw_printf("%f %f %f %f %f %f %f\n", b.bodynode.pos.x, b.bodynode.pos.y, b.bodynode.pos.z, b.vel.x, b.vel.y, b.vel.z, b.bodynode.mass);
+        assert(nbPositionValid(b.bodynode.pos));
+        pushBody(luaSt, &b);
+        lua_rawseti(luaSt, table, i + 1);
+    }
+    
+    
+//         for(int i = 0; i < counter; i++)
+    /* go now and be free!*/
+    free(x);
+    free(y);
+    free(z);
+    free(vx);
+    free(vy);
+    free(vz);
+    free(masses);
+    
+    return 1;             
         
 }
 
 int nbGenerateManualBodies(lua_State* luaSt)
 {
         static const char* body_file = NULL;
-        static real nbodyf = 0.0;
         static const MWNamedArg argTable[] =
         {
-            { "nbody",                LUA_TNUMBER,     NULL,                    TRUE,    &nbodyf            },
             { "body_file",            LUA_TSTRING,     NULL,                    TRUE,    &body_file         },
             END_MW_NAMED_ARG
             
@@ -178,7 +165,7 @@ int nbGenerateManualBodies(lua_State* luaSt)
         
         handleNamedArgumentTable(luaSt, argTable, 1);
         
-        return nbGenerateManualBodiescore(luaSt, (unsigned int) nbodyf, body_file);
+        return nbGenerateManualBodiescore(luaSt, body_file);
 }
 
 
