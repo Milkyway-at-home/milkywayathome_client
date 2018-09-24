3800a313e94b5fe215025adf3d56e1c1d01fa3c3
shelts <shelts@rpi.edu>
2018-09-24 18:05:07 -0400

    new lua file

--- nbody/sample_workunits/EMD_v172.lua
+++ nbody/sample_workunits/EMD_v172.lua
@@ -7,7 +7,7 @@
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 totalBodies           = 20000   -- -- NUMBER OF BODIES           -- --
 nbodyLikelihoodMethod = "EMD"   -- -- HIST COMPARE METHOD        -- --
-nbodyMinVersion       = "1.70"  -- -- MINIMUM APP VERSION        -- --
+nbodyMinVersion       = "1.72"  -- -- MINIMUM APP VERSION        -- --
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
 
