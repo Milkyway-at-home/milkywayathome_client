e60a740cece65bdee0e80581b814a172153515d5
axiomomen <mendee2@rpi.edu>
2018-10-15 16:19:33 -0400

    Began putting in potentials in testing lua file.

--- nbody/tests/SamplePotentials.lua
+++ nbody/tests/SamplePotentials.lua
@@ -23,7 +23,14 @@ require "NBodyTesting"
 local SamplePotentials = { }
 
 SamplePotentials.buildAllDisks = function()
-   local miyamotoNagaiDisks, exponentialDisks
+   local noDisks, miyamotoNagaiDisks, doubleExponentialDisks, sech2ExponentialDisks, freemanDisks
+   noDisks = buildAllCombinations(
+      function(m)
+         return Disk.none{ mass = m }
+      end,
+      { 4.5e5, 5.0e6, 3.0e4 }
+   )
+
    miyamotoNagaiDisks = buildAllCombinations(
       function(m, a, b)
          return Disk.miyamotoNagai{ mass = m, scaleLength = a, scaleHeight = b }
@@ -33,34 +40,124 @@ SamplePotentials.buildAllDisks = function()
       { 0.26, 0.5, 0.1 }
    )
 
-   exponentialDisks = buildAllCombinations(
-      function(m, b)
-         return Disk.exponential{ mass = m, scaleLength = b }
+   doubleExponentialDisks = buildAllCombinations(
+      function(m, a, b)
+         return Disk.exponential{ mass = m, scaleLength = a, scaleHeight = b }
       end,
       { 2.24933e5, 3.0e6, 1.5e4 },
-      { 4, 7, 3 }
+      { 3.6, 2.0, 6.5 },
+      { 0.26, 0.5, 0.1}
    )
 
-   return mergeTables(miyamotoNagaiDisks, exponentialDisks)
+   sech2ExponentialDisks = buildAllCombinations(
+      function(m, a, b)
+         return Disk.exponential{ mass = m, scaleLength = a, scaleHeight = b }
+      end,
+      { 2.24933e5, 3.0e6, 1.5e4 },
+      { 3.6, 2.0, 6.5 },
+      { 0.26, 0.5, 0.1}
+   )
+
+   freemanDisks = buildAllCombinations(
+      function(m, a)
+         return Disk.exponential{ mass = m, scaleLength = a }
+      end,
+      { 2.24933e5, 3.0e6, 1.5e4 },
+      { 3.6, 2.0, 6.5 }
+   )
+
+   return mergeTables(noDisks, miyamotoNagaiDisks, doubleExponentialDisks, sech2ExponentialDisks, freemanDisks)
+end
+
+SamplePotentials.buildAllDisk2s = function()
+   local noDisk2s, miyamotoNagaiDisk2s, doubleExponentialDisk2s, sech2ExponentialDisk2s, freemanDisk2s
+   noDisk2s = buildAllCombinations(
+      function(m)
+         return Disk2.none{ mass = m }
+      end,
+      { 4.5e5, 5.0e6, 3.0e4 }
+   )
+
+   miyamotoNagaiDisk2s = buildAllCombinations(
+      function(m, a, b)
+         return Disk2.miyamotoNagai{ mass = m, scaleLength = a, scaleHeight = b }
+      end,
+      { 4.5e5, 5.0e6, 3.0e4 },
+      { 6.5, 9.0, 3.0 },
+      { 0.26, 0.5, 0.1 }
+   )
+
+   doubleExponentialDisk2s = buildAllCombinations(
+      function(m, a, b)
+         return Disk2.exponential{ mass = m, scaleLength = a, scaleHeight = b }
+      end,
+      { 2.24933e5, 3.0e6, 1.5e4 },
+      { 3.6, 2.0, 6.5 },
+      { 0.26, 0.5, 0.1}
+   )
+
+   sech2ExponentialDisk2s = buildAllCombinations(
+      function(m, a, b)
+         return Disk2.exponential{ mass = m, scaleLength = a, scaleHeight = b }
+      end,
+      { 2.24933e5, 3.0e6, 1.5e4 },
+      { 3.6, 2.0, 6.5 },
+      { 0.26, 0.5, 0.1}
+   )
+
+   freemanDisk2s = buildAllCombinations(
+      function(m, a)
+         return Disk2.exponential{ mass = m, scaleLength = a }
+      end,
+      { 2.24933e5, 3.0e6, 1.5e4 },
+      { 3.6, 2.0, 6.5 }
+   )
+
+   return mergeTables(noDisk2s, miyamotoNagaiDisk2s, doubleExponentialDisk2s, sech2ExponentialDisk2s, freemanDisk2s)
 end
 
 SamplePotentials.buildAllSphericals = function()
-   return buildAllCombinations(
+   local noSphericals, herquistSphericals, plummerSphericals
+   noSphericals = buildAllCombinations(
+      function(m)
+         return Spherical.none{ mass = m }
+      end,
+      { 1.52954402e5, 1.7e6, 1.1e4 }
+   )
+
+   herquistSphericals = buildAllCombinations(
       function(m, s)
-         return Spherical.spherical{ mass = m, scale = s }
+         return Spherical.herquist{ mass = m, scale = s }
       end,
       { 1.52954402e5, 1.7e6, 1.1e4 },
       { 0.7, 1.1, 0.4 }
    )
+
+   plummerSphericals = buildAllCombinations(
+      function(m, s)
+         return Spherical.plummer{ mass = m, scale = s }
+      end,
+      { 1.52954402e5, 1.7e6, 1.1e4 },
+      { 0.7, 1.1, 0.4 }
+   )
+
+   return mergeTables(noSphericals, herquistSphericals, plummerSphericals)
 end
 
 
 SamplePotentials.buildAllHalos = function()
-   local logHalos, nfwHalos, triaxialHalos
+   local noHalos, logHalos, nfwHalos, triaxialHalos, ASHalos, WEHalos, nfwmHalos, plummerHalos, hernquistHalos, ninkovicHalos
+
+   noHalos = buildAllCombinations(
+      function(m)
+         return Halo.none{ mass = m }
+      end,
+      { 1.52954402e5, 1.7e6, 1.1e4 }
+   )
 
    logHalos = buildAllCombinations(
-      function(vhalo, scaleLength, flattenZ)
-         return Halo.logarithmic{ vhalo = vhalo, scaleLength = scaleLength, flattenZ = flattenZ }
+      function(v, L, fZ)
+         return Halo.logarithmic{ vhalo = v, scaleLength = L, flattenZ = fZ }
       end,
       { 73, 100, 50 },
       { 12, 20, 6 },
@@ -68,18 +165,18 @@ SamplePotentials.buildAllHalos = function()
    )
 
    nfwHalos = buildAllCombinations(
-      function(vhalo, scaleLength)
-         return Halo.nfw{ vhalo = vhalo, scaleLength = scaleLength }
+      function(v, L)
+         return Halo.nfw{ vhalo = v, scaleLength = L }
       end,
       { 155, 200, 120 },
       { 22.25, 30, 14 }
    )
 
    triaxialHalos = buildAllCombinations(
-      function(vhalo, scaleLength, flattenZ, flattenX, flattenY, triaxAngle)
-         return Halo.triaxial{ vhalo = vhalo, scaleLength = scaleLength,
-                               flattenZ = flattenZ, flattenX = flattenX, flattenY = flattenY,
-                               triaxAngle = triaxAngle
+      function(v, L, fZ, fX, fY, angle)
+         return Halo.triaxial{ vhalo = v, scaleLength = L,
+                               flattenZ = fZ, flattenX = fX, flattenY = fY,
+                               triaxAngle = angle
                             }
       end,
       { 116, 150, 87 },
@@ -90,19 +187,71 @@ SamplePotentials.buildAllHalos = function()
       { 96, 120, 66 }
    )
 
-   return mergeTables(logHalos, nfwHalos, triaxialHalos)
+   ASHalos = buildAllCombinations(
+      function(m, L, l, g)
+         return Halo.allenSantillan{ mass = m, scaleLength = L, lambda = l, gamma = g}
+      end,
+      { 1.52954402e5, 1.7e6, 1.1e4 },
+      { 16.3, 20, 12 },
+      { 100.0, 200.0, 50.0},
+      { 2.0, 3.0, 4.0 }
+   )
+
+   WEHalos = buildAllCombinations(
+      function(m, L)
+         return Halo.wilkinsonEvans{ mass = m, scaleLength = L }
+      end,
+      { 1.52954402e5, 1.7e6, 1.1e4 },
+      { 16.3, 20, 12 }
+   )
+
+   nfwmHalos = buildAllCombinations(
+      function(m, L)
+         return Halo.nfwmass{ mass = m, scaleLength = L }
+      end,
+      { 1.52954402e5, 1.7e6, 1.1e4 },
+      { 16.3, 20, 12 }
+   )
+
+   plummerHalos = buildAllCombinations(
+      function(m, L)
+         return Halo.plummer{ mass = m, scaleLength = L }
+      end,
+      { 1.52954402e5, 1.7e6, 1.1e4 },
+      { 16.3, 20, 12 }
+   )
+
+   hernquistHalos = buildAllCombinations(
+      function(m, L)
+         return Halo.hernquist{ mass = m, scaleLength = L }
+      end,
+      { 1.52954402e5, 1.7e6, 1.1e4 },
+      { 16.3, 20, 12 }
+   )
+
+   ninkovicHalos = buildAllCombinations(
+      function(p, L, l)
+         return Halo.ninkovic{ rho0 = p, scaleLength = L, lambda = l }
+      end,
+      { 1.52954402e5, 1.7e6, 1.1e4 },
+      { 16.3, 20, 12 },
+      { 70.0, 96.5, 150.0}
+   )
+
+   return mergeTables(noHalos, logHalos, nfwHalos, triaxialHalos, ASHalos, WEHalos, nfwmHalos, plummerHalos, hernquistHalos, ninkovicHalos)
 end
 
 SamplePotentials.makeAllPotentials = function()
   return buildAllCombinations(Potential.create,
                               SamplePotentials.buildAllSphericals(),
                               SamplePotentials.buildAllDisks(),
+                              SamplePotentials.buildAllDisk2s(),
                               SamplePotentials.buildAllHalos())
 
 end
 
 -- Artificial tests, should have at least every combination of
--- spherical, disk, halo component types.
+-- spherical, disk, halo component typs. (750 COMBINATIONS!?!)
 SamplePotentials.samplePotentials = {
    potentialA = Potential.create{
       spherical = Spherical.spherical{
@@ -227,20 +376,24 @@ SamplePotentials.randomHalo = function(prng)
       prng = DSFMT.create()
    end
 
-   local type = floor(prng:random(0, 3))
+   local typ = floor(prng:random(0, 10))
 
-   if type == 0 then
+   if typ == 0 then
+      return Halo.none{
+         mass        = prng:random(1.0e4, 5.0e7)
+      }
+   elseif typ == 1 then
       return Halo.logarithmic{
          vhalo       = prng:random(10, 200),
          scaleLength = prng:random(3, 100),
          flattenZ    = prng:random(0, 3)
       }
-   elseif type == 1 then
+   elseif typ == 2 then
       return Halo.nfw{
          vhalo       = prng:random(1, 300),
          scaleLength = prng:random(0.1, 40)
       }
-   elseif type == 2 then
+   elseif typ == 3 then
       return Halo.triaxial{
          vhalo       = prng:random(1, 200),
          scaleLength = prng:random(0.1, 30),
@@ -248,6 +401,39 @@ SamplePotentials.randomHalo = function(prng)
          flattenY    = prng:random(0, 3),
          flattenZ    = prng:random(0, 4),
          triaxAngle  = prng:random(0, 180)
+      }
+   elseif typ == 4 then
+      return Halo.allenSantillan{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(0.1, 30),
+         lambda      = prng:random(20.0, 300.0),
+         gamma       = prng:random(2.0, 5.0)
+      }
+   elseif typ == 5 then
+      return Halo.wilkinsonEvans{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(0.1, 30)
+      }
+   elseif typ == 6 then
+      return Halo.nfwmass{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(0.1, 30)
+      }
+   elseif typ == 7 then
+      return Halo.plummer{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(0.1, 30)
+      }
+   elseif typ == 8 then
+      return Halo.hernquist{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(0.1, 30)
+      }
+   elseif typ == 9 then
+      return Halo.ninkovic{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(0.1, 30),
+         lambda      = prng:random(30.0, 150.0)
       }
    else
       assert(false)
@@ -259,18 +445,73 @@ SamplePotentials.randomDisk = function(prng)
       prng = DSFMT.create()
    end
 
-   local type = floor(prng:random(0, 2))
+   local typ = floor(prng:random(0, 5))
 
-   if type == 0 then
+   if typ == 0 then
+      return Disk.none{
+         mass        = prng:random(1.0e4, 5.0e7)
+      }
+   elseif typ == 1 then
       return Disk.miyamotoNagai{
          mass        = prng:random(1.0e4, 5.0e7),
          scaleLength = prng:random(1, 10),
-         scaleHeight = prng:random(1, 10)
+         scaleHeight = prng:random(0.1, 8)
       }
-   elseif type == 1 then
-      return Disk.exponential{
+   elseif typ == 2 then
+      return Disk.doubleExponential{
          mass        = prng:random(1.0e4, 5.0e7),
-         scaleLength = prng:random(1, 12)
+         scaleLength = prng:random(1, 10),
+         scaleHeight = prng:random(0.1, 8)
+      }
+   elseif typ == 3 then
+      return Disk.sech2Exponential{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(1, 10),
+         scaleHeight = prng:random(0.1, 8)
+      }
+   elseif typ == 4 then
+      return Disk.freeman{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(1, 10)
+      }
+   else
+      assert(false)
+   end
+end
+
+SamplePotentials.randomDisk2 = function(prng)
+   if prng == nil then
+      prng = DSFMT.create()
+   end
+
+   local typ = floor(prng:random(0, 5))
+
+   if typ == 0 then
+      return Disk2.none{
+         mass        = prng:random(1.0e4, 5.0e7)
+      }
+   elseif typ == 1 then
+      return Disk2.miyamotoNagai{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(1, 10),
+         scaleHeight = prng:random(0.1, 8)
+      }
+   elseif typ == 2 then
+      return Disk2.doubleExponential{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(1, 10),
+         scaleHeight = prng:random(0.1, 8)
+      }
+   elseif typ == 3 then
+      return Disk2.sech2Exponential{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(1, 10),
+         scaleHeight = prng:random(0.1, 8)
+      }
+   elseif typ == 4 then
+      return Disk2.freeman{
+         mass        = prng:random(1.0e4, 5.0e7),
+         scaleLength = prng:random(1, 10)
       }
    else
       assert(false)
@@ -282,16 +523,32 @@ SamplePotentials.randomSpherical = function(prng)
       prng = DSFMT.create()
    end
 
-   return Spherical.spherical{
-      mass  = prng:random(1.0e4, 9e7),
-      scale = prng:random(0, 4)
-   }
+   local typ == floor(prng:random(0, 3))
+
+   if typ == 0 then
+      return Spherical.none{
+         mass        = prng:random(1.0e4, 5.0e7)
+      }
+   elseif typ == 1 then
+      return Spherical.hernquist{
+         mass  = prng:random(1.0e4, 9e7),
+         scale = prng:random(0, 4)
+      }
+   elseif typ == 2 then
+      return Spherical.plummer{
+         mass  = prng:random(1.0e4, 9e7),
+         scale = prng:random(0, 4)
+      }
+   else
+      assert(false)
+   end
 end
 
 SamplePotentials.randomPotential = function(prng)
    return Potential.create{
       spherical = SamplePotentials.randomSpherical(prng),
       disk      = SamplePotentials.randomDisk(prng),
+      disk2     = SamplePotentials.randomDisk2(prng),
       halo      = SamplePotentials.randomHalo(prng)
    }
 end
