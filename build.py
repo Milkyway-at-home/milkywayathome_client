0d3d126c320d46bbe1a4906f1b3d4bbb2b529d34
Jake Weiss <weissj3@rpi.edu>
2019-04-17 09:34:09 -0400

    Fix Variable name

--- build.py
+++ build.py
@@ -30,7 +30,7 @@ assert arch in ["32", "64"], "ERROR: Unknown arch " + bit
 assert sep_opencl in ["ON", "OFF"], "ERROR: Set SEP_OPENCL to ON or OFF" #No longer using multithreaded
 
 # CMake flags used for all platforms
-cmake_shared_flags = ["-DSEPARATION=ON", "-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER", "-DNBODY=" + sep_opencl, "-DSEPARATION_OPENCL=" + nbody_openmp_sep_opencl]
+cmake_shared_flags = ["-DSEPARATION=ON", "-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER", "-DNBODY=" + sep_opencl, "-DSEPARATION_OPENCL=" + sep_opencl]
 
 # CMake flags used for windows
 cmake_static_flag = ["-DNBODY_STATIC=ON",  "-DBOINC_APPLICATION=ON",  "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_TOOLCHAIN_FILE=cmake_modules/MinGW32-Cross-Toolchain.cmake"]
