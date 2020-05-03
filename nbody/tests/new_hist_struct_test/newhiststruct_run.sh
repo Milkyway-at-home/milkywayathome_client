e6b835261c3f2d961a871900ea9d85ce32f32ff6
Emily Crook <ehcrook@gmail.com>
2020-05-03 18:28:08 -0400

    test fix

--- nbody/tests/new_hist_struct_test/newhiststruct_run.sh
+++ nbody/tests/new_hist_struct_test/newhiststruct_run.sh
@@ -7,5 +7,5 @@ cd /mnt/c/users/"Emily Crook"/desktop/new_hist_struct/milkywayathome_client/buil
 -f /mnt/c/users/"Emily Crook"/desktop/new_hist_struct/milkywayathome_client/nbody/tests/new_hist_struct_test/newhiststruct_for_developers.lua \
 -z /mnt/c/users/"Emily Crook"/desktop/new_hist_struct/milkywayathome_client/nbody/tests/new_hist_struct_test/input_hist.hist \
 -o /mnt/c/users/"Emily Crook"/desktop/new_hist_struct/milkywayathome_client/nbody/tests/new_hist_struct_test/input_out.out \
--n 8 -b -e $13\
--i $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11\
+-n 8 -b -e $12\
+-i $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12\
