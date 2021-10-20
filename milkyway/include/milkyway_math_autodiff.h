fa184bb4dd9cca56f1d5cf7891e8002b7047fc35
Eric Mendelsohn <mendee2@rpi.edu>
2021-10-20 17:10:06 -0400

    Added new code to distinguish between builds with AUTODIFF and without

---
+++ milkyway/include/milkyway_math_autodiff.h
@@ -0,0 +1,236 @@
+/*
+ *  Copyright (c) 2010-2022 Rensselaer Polytechnic Institute
+ *  Copyright (c) 2018-2022 Eric Mendelsohn
+ *
+ *  This file is part of Milkway@Home.
+ *
+ *  Milkway@Home is free software: you may copy, redistribute and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ *
+ *  This file is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined(_MILKYWAY_MATH_H_INSIDE_) && !defined(MILKYWAY_MATH_COMPILATION)
+  #error "Only milkyway_math.h can be included directly."
+#endif
+
+#ifndef _MILKYWAY_MATH_AUTODIFF_H_
+#define _MILKYWAY_MATH_AUTODIFF_H_
+
+#include "milkyway_extra.h"
+
+#ifndef DOUBLEPREC
+  #error DOUBLEPREC not defined
+#endif
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if AUTODIFF
+    int NumberOfModelParameters = 20     /*Change this number to add more space for parameters to differentiate over*/
+
+    typedef struct MW_ALIGN_TYPE_V(sizeof(real_0)*(1 + NumberOfModelParameters + NumberOfModelParameters*NumberOfModelParameters))
+    {
+        real_0 value;
+        real_0 gradient[NumberOfModelParameters];
+        real_0 hessian[NumberOfModelParameters][NumberOfModelParameters];
+    } real;
+
+    real_0 ZERO_GRADIENT[NumberOfModelParameters] = {0.0};
+    real_0 ZERO_HESSIAN[NumberOfModelParameters][NumberOfModelParameters] = {0.0};
+    #define ZERO_REAL {0.0, ZERO_GRADIENT, ZERO_HESSIAN}
+
+    CONST_F ALWAYS_INLINE
+    static inline real mw_real_const(real_0 a)
+    {
+        real result;
+	result.value = a;
+        result.gradient = ZERO_GRADIENT;
+        result.hessian = ZERO_HESSIAN;
+        return result;
+    }
+    #define INIT_REAL_CONST(r,x) {(r).value = (x); (r).gradient = ZERO_GRADIENT; (r).hessian = ZERO_HESSIAN;}
+    #define INIT_REAL_VAR(r,x,n) {INIT_REAL_CONST((r),(x)); (r).gradient[n] = 1.0}
+
+#else
+
+    typedef struct MW_ALIGN_TYPE_V(sizeof(real_0)) real_0 real;
+
+    #define ZERO_REAL 0.0
+
+    CONST_F ALWAYS_INLINE
+    static inline real mw_real_const(real a)
+    {
+        return a;
+    }
+    #define INIT_REAL_CONST(r,x) {(r) = (x);}
+    #define INIT_REAL_VAR(r,x,n) {INIT_REAL_CONST((r),(x));}
+
+    CONST_F ALWAYS_INLINE
+    static inline real mw_add(real a, real b)
+    {
+        return (a+b);
+    }
+
+    CONST_F ALWAYS_INLINE
+    static inline real mw_sub(real a, real b)
+    {
+        return (a-b);
+    }
+
+    CONST_F ALWAYS_INLINE
+    static inline real mw_mul(real a, real b)
+    {
+        return (a*b);
+    }
+
+    CONST_F ALWAYS_INLINE
+    static inline real mw_div(real a, real b)
+    {
+        return (a/b);
+    }
+
+    #define mw_sin   mw_sin_0
+    #define mw_cos   mw_cos_0
+    #define mw_tan   mw_tan_0
+    #define mw_asin  mw_asin_0
+    #define mw_acos  mw_acos_0
+    #define mw_atan  mw_atan_0
+
+    #define mw_log   mw_log_0
+    #define mw_log1p mw_log1p_0
+    #define mw_exp   mw_exp_0
+    #define mw_expm1 mw_expm1_0
+    #define mw_log10 mw_log10_0
+    #define mw_sinh  mw_sinh_0
+    #define mw_cosh  mw_cosh_0
+    #define mw_tanh  mw_tanh_0
+    #define mw_pow   mw_pow_0
+
+    #define mw_abs   mw_fabs
+
+    #define mw_acosh    mw_acosh_0
+    #define mw_acospi   mw_acospi_0
+    #define mw_asinh    mw_asinh_0
+    #define mw_asinpi   mw_asinpi_0
+    #define mw_atan2    mw_atan2_0
+    #define mw_atanh    mw_atanh_0
+    #define mw_atanpi   mw_atanpi_0
+    #define mw_atan2pi  mw_atan2pi_0
+    #define mw_cbrt     mw_cbrt_0
+    #define mw_ceil     mw_ceil_0
+    #define mw_copysign mw_copysign_0
+    #define mw_cospi    mw_cospi_0
+    #define mw_sinpi    mw_sinpi_0
+    #define mw_tanpi    mw_tanpi_0
+    #define mw_erfc     mw_erfc_0
+    #define mw_erf      mw_erf_0
+
+    #define mw_exp2     mw_exp2_0
+    #define mw_exp10    mw_exp10_0
+
+    #define mw_fabs     mw_fabs_0
+    #define mw_fdim     mw_fdim_0
+    #define mw_floor    mw_floor_0
+
+    #define mw_fmod     mw_fmod_0
+
+    #define mw_fract    mw_fract_0
+
+    #define mw_frexp     mw_frexp_0
+    #define mw_ilogb     mw_ilogb_0
+    #define mw_ldexp     mw_ldexp_0
+    #define mw_tgamma    mw_tgamma_0
+    #define mw_tgamma_r  mw_tgamma_r_0
+    #define mw_lgamma    mw_lgamma_0
+    #define mw_lgamma_r  mw_lgamma_r_0
+    #define mw_log2      mw_log2_0
+    #define mw_logb      mw_logb_0
+    #define mw_mad       mw_mad_0
+    #define mw_modf      mw_modf_0
+    #define mw_nan       mw_nan_0
+    #define mw_nextafter mw_nextafter_0
+
+    #define mw_powr      mw_powr_0
+
+    #define mw_remainder mw_remainder_0
+    #define mw_remquo    mw_remquo_0
+    #define mw_rint      mw_rint_0
+    #define mw_rootn     mw_rootn_0
+    #define mw_round     mw_round_0
+
+    #define mw_sqrt      mw_sqrt_0
+    #define mw_trunc     mw_trunc_0
+
+    #define sixth        sixth_0
+    #define fifth        fifth_0
+    #define fourth       fourth_0
+    #define cube         cube_0
+    #define sqr          sqr_0
+    #define inv          inv_0
+
+    #define fivehalves   fivehalves_0
+    #define threehalves  threehalves_0
+
+    #define minusfivehalves  minusfivehalves_0
+    #define minusthreehalves minusthreehalves_0
+    #define minushalf        minushalf_0
+
+    #define mw_fma       mw_fma_0
+    #define mw_hypot     mw_hypot_0
+
+    #define mw_fmax   mw_fmax_0
+    #define mw_fmin   mw_fmin_0
+    #define mw_rsqrt  mw_rsqrt_0
+    #define mw_sincos mw_sincos_0
+
+    #define   ABS     ABS_0
+    #define   MAX     MAX_0
+    #define   MIN     MIN_0
+
+    #define mw_cmpzero_machineeps  mw_cmpzero_machineeps_0
+    #define mw_cmpnzero_machineeps mw_cmpnzero_machineeps_0
+
+    #define mw_cmpzero_eps     mw_cmpzero_eps_0
+    #define mw_cmpnzero_eps    mw_cmpnzero_eps_0
+
+    #define mw_cmpzero_muleps  mw_cmpzero_muleps_0
+    #define mw_cmpnzero_muleps mw_cmpnzero_muleps_0
+
+    #define mw_cmpf    mw_cmpf_0
+    #define mw_cmpnf   mw_cmpnf_0
+
+    #define d2r   d2r_0
+    #define r2d   r2d_0
+    #define dsign dsign_0
+
+
+#endif /*AUTODIFF*/
+
+#if DOUBLEPREC
+    typedef MW_ALIGN_TYPE_V(2*sizeof(real)) real double2[2];
+    typedef MW_ALIGN_TYPE_V(4*sizeof(real)) real double4[4];
+#else
+    typedef MW_ALIGN_TYPE_V(2*sizeof(real)) real float2[2];
+    typedef MW_ALIGN_TYPE_V(4*sizeof(real)) real float4[4];
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _MILKYWAY_MATH_AUTODIFF_H_ */
--- milkyway/include/milkyway_vectors.h
+++ milkyway/include/milkyway_vectors.h
@@ -45,14 +45,14 @@ typedef struct MW_ALIGN_TYPE_V(4 * sizeof(real))
 #define CZ(v) ((v).z)
 
 
-#define mw_vec(x, y, z) { (x), (y), (z), 0.0 }
+#define mw_vec(x, y, z) { (x), (y), (z), ZERO_REAL }
 
 #define SET_VECTOR(v, x, y, z) { X(v) = (x); Y(v) = (y); Z(v) = (z); }
 
 
 #define NDIM 3
 
-#define ZERO_VECTOR { 0.0, 0.0, 0.0, 0.0 }
+#define ZERO_VECTOR { ZERO_REAL, ZERO_REAL, ZERO_REAL, ZERO_REAL }
 typedef mwvector mwmatrix[NDIM];
 #define ZERO_MATRIX { ZERO_VECTOR, ZERO_VECTOR, ZERO_VECTOR }
 
@@ -63,9 +63,9 @@ CONST_F
 static inline mwvector mw_addv(mwvector a, mwvector b)
 {
     mwvector v;
-    v.x = a.x + b.x;
-    v.y = a.y + b.y;
-    v.z = a.z + b.z;
+    v.x = mw_add(a.x, b.x);
+    v.y = mw_add(a.y, b.y);
+    v.z = mw_add(a.z, b.z);
     return v;
 }
 
@@ -73,9 +73,9 @@ CONST_F
 static inline mwvector mw_subv(mwvector a, mwvector b)
 {
     mwvector v;
-    v.x = a.x - b.x;
-    v.y = a.y - b.y;
-    v.z = a.z - b.z;
+    v.x = mw_sub(a.x, b.x);
+    v.y = mw_sub(a.y, b.y);
+    v.z = mw_sub(a.z, b.z);
 
     return v;
 }
@@ -84,9 +84,9 @@ CONST_F
 static inline mwvector mw_mulv(mwvector a, mwvector b)
 {
     mwvector v;
-    v.x = a.x * b.x;
-    v.y = a.y * b.y;
-    v.z = a.z * b.z;
+    v.x = mw_mul(a.x, b.x);
+    v.y = mw_mul(a.y, b.y);
+    v.z = mw_mul(a.z, b.z);
 
     return v;
 }
@@ -95,9 +95,9 @@ CONST_F
 static inline mwvector mw_divv(mwvector a, mwvector b)
 {
     mwvector v;
-    v.x = a.x / b.x;
-    v.y = a.y / b.y;
-    v.z = a.z / b.z;
+    v.x = mw_div(a.x, b.x);
+    v.y = mw_div(a.y, b.y);
+    v.z = mw_div(a.z, b.z);
 
     return v;
 }
@@ -105,28 +105,28 @@ static inline mwvector mw_divv(mwvector a, mwvector b)
 CONST_F
 static inline real mw_dotv(mwvector a, mwvector b)
 {
-    return mw_mad(a.z, b.z, mw_mad(a.y, b.y, a.x * b.x));
+    return mw_mad(a.z, b.z, mw_mad(a.y, b.y, mw_mul(a.x, b.x)));
 }
 
 CONST_F
 static inline mwvector mw_crossv(mwvector a, mwvector b)
 {
-    mwvector tmp = mw_vec(b.z * a.y - b.y * a.z,
-                          b.x * a.z - b.z * a.x,
-                          b.y * a.x - b.x * a.y);
+    mwvector tmp = mw_vec(mw_sub(mw_mul(b.z, a.y), mw_mul(b.y, a.z)),
+                          mw_sub(mw_mul(b.x, a.z), mw_mul(b.z, a.x)),
+                          mw_sub(mw_mul(b.y, a.x), mw_mul(b.x, a.y));
     return tmp;
 }
 
 CONST_F
 static inline real mw_length(mwvector a)
 {
-    return mw_sqrt(sqr(a.x) + sqr(a.y) + sqr(a.z));
+    return mw_sqrt(mw_add(mw_add(sqr(a.x), sqr(a.y)), sqr(a.z)));
 }
 
 CONST_F
 static inline real mw_sqrv(mwvector a)
 {
-    return mw_mad(a.z, a.z, mw_mad(a.y, a.y, a.x * a.x));
+    return mw_mad(a.z, a.z, mw_mad(a.y, a.y, sqr(a.x)));
 }
 
 CONST_F
@@ -139,9 +139,9 @@ CONST_F
 static inline mwvector mw_mulvs(mwvector a, real s)
 {
     mwvector v;
-    v.x =  s * a.x;
-    v.y =  s * a.y;
-    v.z =  s * a.z;
+    v.x =  mw_mul(s, a.x);
+    v.y =  mw_mul(s, a.y);
+    v.z =  mw_mul(s, a.z);
     return v;
 }
 
@@ -149,9 +149,9 @@ CONST_F
 static inline mwvector mw_divvs(mwvector a, real s)
 {
     mwvector v;
-    v.x = a.x / s;
-    v.y = a.y / s;
-    v.z = a.z / s;
+    v.x = mw_div(a.x, s);
+    v.y = mw_div(a.y, s);
+    v.z = mw_div(a.z, s);
     return v;
 }
 
@@ -159,9 +159,9 @@ CONST_F
 static inline mwvector mw_negv(mwvector a)
 {
     mwvector v;
-    v.x = -a.x;
-    v.y = -a.y;
-    v.z = -a.z;
+    v.x = mw_mul(mw_real_const(-1.0),a.x);
+    v.y = mw_mul(mw_real_const(-1.0),a.y);
+    v.z = mw_mul(mw_real_const(-1.0),a.z);
     return v;
 }
 
@@ -178,7 +178,7 @@ static inline mwvector mw_mulmv(const mwmatrix m, mwvector a)
 CONST_F
 static inline real mw_distv(mwvector u, mwvector v)
 {
-    return mw_sqrt(sqr(u.x - v.x) + sqr(u.y - v.y) + sqr(u.z - v.z));
+    return mw_sqrt(mw_add(mw_add(sqr(mw_sub(u.x, v.x)), sqr(mw_sub(u.y, v.y))), sqr(mw_sub(u.z, v.z))));
 }
 
 /* Angle between two vectors, in the range [0,pi] */
@@ -191,117 +191,104 @@ static inline real mw_vecangle(mwvector a, mwvector b)
     bnorm = mw_length(b);
     dot = mw_dotv(a, b);
 
-    return mw_acos(dot / (anorm * bnorm));
+    return mw_acos(mw_div(dot, mw_mul(anorm, bnorm)));
 }
 
 
-#define mw_incsubv(v1, v2) { (v1).x -= (v2).x; (v1).y -= (v2).y; (v1).z -= (v2).z; }
-#define mw_incaddv(v1, v2) { (v1).x += (v2).x; (v1).y += (v2).y; (v1).z += (v2).z; }
-#define mw_incdivv(v1, v2) { (v1).x /= (v2).x; (v1).y /= (v2).y; (v1).z /= (v2).z; }
-#define mw_incmulv(v1, v2) { (v1).x *= (v2).x; (v1).y *= (v2).y; (v1).z *= (v2).z; }
-#define mw_incnegv(v1) { (v1).x = -(v1).x; (v1).y = -(v1).y; (v1).z = -(v1).z; }
-#define mw_zerov(v) { (v).x = 0.0; (v).y = 0.0; (v).z = 0.0; }
+#define mw_incsubv(v1, v2) { (v1).x = mw_sub((v1).x, (v2).x); (v1).y = mw_sub((v1).y, (v2).y); (v1).z = mw_sub((v1).z, (v2).z); }
+#define mw_incaddv(v1, v2) { (v1).x = mw_add((v1).x, (v2).x); (v1).y = mw_add((v1).y, (v2).y); (v1).z = mw_add((v1).z, (v2).z); }
+#define mw_incdivv(v1, v2) { (v1).x = mw_div((v1).x, (v2).x); (v1).y = mw_div((v1).y, (v2).y); (v1).z = mw_div((v1).z, (v2).z); }
+#define mw_incmulv(v1, v2) { (v1).x = mw_mul((v1).x, (v2).x); (v1).y = mw_mul((v1).y, (v2).y); (v1).z = mw_mul((v1).z, (v2).z); }
+#define mw_incnegv(v1) { (v1).x = mw_mul(mw_real_const(-1.0),(v1).x); (v1).y = mw_mul(mw_real_const(-1.0),(v1).y); (v1).z = mw_mul(mw_real_const(-1.0),(v1).z); }
+#define mw_zerov(v) { (v).x = ZERO_REAL; (v).y = ZERO_REAL; (v).z = ZERO_REAL; }
 
 /* v1 += s * v2 */
-#define mw_incaddv_s(v1, v2, s) { (v1).x += (s) * (v2).x; (v1).y += (s) * (v2).y; (v1).z += (s) * (v2).z; }
-
-#if !USE_MAD
+#define mw_incaddv_s(v1, v2, s) { mw_incaddv((v1),mw_mulvs((v2),(s))); }
 
 /* v1 -= s * v2 */
-#define mw_incsubv_s(v1, v2, s) { (v1).x -= (s) * (v2).x; (v1).y -= (s) * (v2).y; (v1).z -= (s) * (v2).z; }
+#define mw_incsubv_s(v1, v2, s) { mw_incsubv((v1),mw_mulvs((v2),(s))); }
 
-#else
+#define mw_incdivs(v, s) { (v).x = mw_div((v).x,(s)); (v).y = mw_div((v).y,(s)); (v).z = mw_div((v).z,(s)); }
+#define mw_incmulvs(v, s) { (v).x = mw_mul((v).x,(s)); (v).y = mw_mul((v).y,(s)); (v).z = mw_mul((v).z,(s)); }
 
-#define mw_incsubv_s(v1, v2, s)                 \
-    {                                           \
-        (v1).x = mw_mad(-(s), (v2).x, (v1).x);  \
-        (v1).y = mw_mad(-(s), (v2).y, (v1).y);  \
-        (v1).z = mw_mad(-(s), (v2).z, (v1).z);  \
-    }
-
-#endif /* USE_MAD */
-
-#define mw_incdivs(v, s) { (v).x /= (s); (v).y /= (s); (v).z /= (s); }
-#define mw_incmulvs(v, s) { (v).x *= (s); (v).y *= (s); (v).z *= (s); }
-
-#define mw_normalize(v) { real len = mw_length(v); (v).x /= len; (v).y /= len; (v).z /= len; }
+#define mw_normalize(v) { real len = mw_length(v); mw_incdivs(v, len); }
 
 /* Outer product */
 
 static inline void mw_outv(mwmatrix p, mwvector v, mwvector u)
 {
-    X(p[0]) = X(v) * X(u);
-    Y(p[0]) = X(v) * Y(u);
-    Z(p[0]) = X(v) * Z(u);
+    X(p[0]) = mw_mul(X(v), X(u));
+    Y(p[0]) = mw_mul(X(v), Y(u));
+    Z(p[0]) = mw_mul(X(v), Z(u));
 
-    X(p[1]) = Y(v) * X(u);
-    Y(p[1]) = Y(v) * Y(u);
-    Z(p[1]) = Y(v) * Z(u);
+    X(p[1]) = mw_mul(Y(v), X(u));
+    Y(p[1]) = mw_mul(Y(v), Y(u));
+    Z(p[1]) = mw_mul(Y(v), Z(u));
 
-    X(p[2]) = Z(v) * X(u);
-    Y(p[2]) = Z(v) * Y(u);
-    Z(p[2]) = Z(v) * Z(u);
+    X(p[2]) = mw_mul(Z(v), X(u));
+    Y(p[2]) = mw_mul(Z(v), Y(u));
+    Z(p[2]) = mw_mul(Z(v), Z(u));
 }
 
 /* Outer product of a vector with itself */
 static inline void mw_outsqrv(mwmatrix p, mwvector v)
 {
-    X(p[0]) = X(v) * X(v);
-    Y(p[0]) = X(v) * Y(v);
-    Z(p[0]) = X(v) * Z(v);
+    X(p[0]) = mw_mul(X(v), X(v));
+    Y(p[0]) = mw_mul(X(v), Y(v));
+    Z(p[0]) = mw_mul(X(v), Z(v));
 
     X(p[1]) = Y(p[0]);
-    Y(p[1]) = Y(v) * Y(v);
-    Z(p[1]) = Y(v) * Z(v);
+    Y(p[1]) = mw_mul(Y(v), Y(v));
+    Z(p[1]) = mw_mul(Y(v), Z(v));
 
     X(p[2]) = Z(p[0]);
     Y(p[2]) = Z(p[1]);
-    Z(p[2]) = Z(v) * Z(v);
+    Z(p[2]) = mw_mul(Z(v), Z(v));
 }
 
 static inline void mw_set_diagonal_matrix(mwmatrix p, real s)
 {
     X(p[0]) = s;
-    Y(p[0]) = 0.0;
-    Z(p[0]) = 0.0;
+    Y(p[0]) = ZERO_REAL;
+    Z(p[0]) = ZERO_REAL;
 
-    X(p[1]) = 0.0;
+    X(p[1]) = ZERO_REAL;
     Y(p[1]) = s;
-    Z(p[1]) = 0.0;
+    Z(p[1]) = ZERO_REAL;
 
-    X(p[2]) = 0.0;
-    Y(p[2]) = 0.0;
+    X(p[2]) = ZERO_REAL;
+    Y(p[2]) = ZERO_REAL;
     Z(p[2]) = s;
 }
 
 static inline void mw_set_matrix_identity(mwmatrix p)
 {
-    X(p[0]) = 1.0;
-    Y(p[0]) = 0.0;
-    Z(p[0]) = 0.0;
+    X(p[0]) = mw_real_const(1.0);
+    Y(p[0]) = ZERO_REAL;
+    Z(p[0]) = ZERO_REAL;
 
-    X(p[1]) = 0.0;
-    Y(p[1]) = 1.0;
-    Z(p[1]) = 0.0;
+    X(p[1]) = ZERO_REAL;
+    Y(p[1]) = mw_real_const(1.0);
+    Z(p[1]) = ZERO_REAL;
 
-    X(p[2]) = 0.0;
-    Y(p[2]) = 0.0;
-    Z(p[2]) = 1.0;
+    X(p[2]) = ZERO_REAL;
+    Y(p[2]) = ZERO_REAL;
+    Z(p[2]) = mw_real_const(1.0);
 }
 
 static inline void mw_set_matrix_zero(mwmatrix p)
 {
-    X(p[0]) = 0.0;
-    Y(p[0]) = 0.0;
-    Z(p[0]) = 0.0;
+    X(p[0]) = ZERO_REAL;
+    Y(p[0]) = ZERO_REAL;
+    Z(p[0]) = ZERO_REAL;
 
-    X(p[1]) = 0.0;
-    Y(p[1]) = 0.0;
-    Z(p[1]) = 0.0;
+    X(p[1]) = ZERO_REAL;
+    Y(p[1]) = ZERO_REAL;
+    Z(p[1]) = ZERO_REAL;
 
-    X(p[2]) = 0.0;
-    Y(p[2]) = 0.0;
-    Z(p[2]) = 0.0;
+    X(p[2]) = ZERO_REAL;
+    Y(p[2]) = ZERO_REAL;
+    Z(p[2]) = ZERO_REAL;
 }
 
 static inline void mw_addm(mwmatrix p, mwmatrix q, mwmatrix r)
