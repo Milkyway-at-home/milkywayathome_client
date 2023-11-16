0ca2cd8da36a351e5381a7d33fbb86820d9169bf
Kevnroux <kevroux3@utexas.edu>
2023-11-16 03:51:44 -0500

    Quality of life changes which removes necessity for hard coded directories and seperation of building script and nbody running script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#/* Copyright (c) 2023 Kevin Roux */
+
+cd "$(dirname "$0")"
+PathToMilkyWayAtHomeClientDirectory="$(pwd)"
+echo "Path to milkywayathome_client directory: $PathToMilkyWayAtHomeClientDirectory"
+
+rm -r build
+mkdir build
+cd build
+cmake  -DCMAKE_BUILD_TYPE=Release -DNBODY_DEV_OPTIONS=ON -DNBODY_GL=OFF -DBOINC_APPLICATION=ON -DSEPARATION=OFF -DDOUBLEPREC=ON -DNBODY_OPENMP=ON -DNBODY_OPENCL=OFF $PathToMilkyWayAtHomeClientDirectory/
+make -j 
---
+++ run_nbody.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#/* Copyright (c) 2023 Kevin Roux */
+
+run=true
+run_compare=false
+compare_only=false
+get_flag_list=false
+
+cd "$(dirname "$0")"
+PathToMilkyWayAtHomeClientDirectory="$(pwd)"
+echo "Path to milkywayathome_client directory: $PathToMilkyWayAtHomeClientDirectory"
+
+cd build/bin
+
+if $run 
+then
+    ./milkyway_nbody \
+    -f $PathToMilkyWayAtHomeClientDirectory/nbody/sample_workunits/for_developers.lua \
+    -o $PathToMilkyWayAtHomeClientDirectory/output/output.out \
+    -z $PathToMilkyWayAtHomeClientDirectory/output/output.hist \
+    -n 8 -b -w 1 -P -e 54231651 \
+    -i 4.0 1.0 0.2 0.2 12.0 0.2 \
+    
+fi
+
+if $run_compare
+then
+    ./milkyway_nbody \
+    -f $PathToMilkyWayAtHomeClientDirectory/nbody/sample_workunits/for_developers.lua \
+    -o $PathToMilkyWayAtHomeClientDirectory/output/output.out \
+    -z $PathToMilkyWayAtHomeClientDirectory/output/output.hist \
+    -h $PathToMilkyWayAtHomeClientDirectory/input/input.hist \
+    -n 8 -b -w 1 -P -e 54231651 \
+    -p 4.0 1.0 0.2 0.2 12.0 0.2 \
+
+fi
+
+#SMU = 222,288.47 SOLAR MASSES
+
+#OPTIONS:
+#-s -> compare using only emd and cost component
+#-S -> use emd, cost, beta dispersion
+#-V -> use emd, cost, velocity dispersion
+#-D -> use emd, cost, beta dispersion and velocity dispersion
+if $compare_only 
+then
+    ./milkyway_nbody \
+    -h $PathToMilkyWayAtHomeClientDirectory/inputs/test_nbody100000.hist \
+    -S $PathToMilkyWayAtHomeClientDirectory/outputs/resultstemp.hist \
+
+fi
+
+# if you run:
+
+if $get_flag_list
+then
+    ./milkyway_nbody --help
+# it will show you what all the flags mean
+fi
