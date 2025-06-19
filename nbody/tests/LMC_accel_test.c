578707bf186de509d4a1fa87ab8ac61f61540df9
Kevnroux <rouxk@rpi.edu>
2025-06-19 11:54:11 -0400

    LMC test now compiles

--- nbody/tests/LMC_accel_test.c
+++ nbody/tests/LMC_accel_test.c
@@ -13,14 +13,18 @@ int test_LMC_accel()
     mwvector LMCx = mw_vec(0, 0, 0);
     real LMCmass = 202439.64970382853;
 
+    //declare acceleration vectors and tolerance
+    mwvector A1, A2, A3, A4;
+    real delta;
+
     //call LMC functions
     A1 = LMCAcceleration(1, x, LMCx, LMCmass, 8.32, 1.0);//Plummer LMC
     A2 = LMCAcceleration(2, x, LMCx, LMCmass, 5.45, 1.0);//Henrquist LMC
-    A3 = LMCAcceleration(3, mw_vec(5, 5, 5), LMCx, 2336886.4425964956, 39.8, 16.6);//before cutoff 
+    A3 = LMCAcceleration(3, (mwvector){5, 5, 5, 0.0}, LMCx, 2336886.4425964956, 39.8, 16.6);//before cutoff 
     A4 = LMCAcceleration(3, x, LMCx, 2336886.4425964956, 39.8, 16.6);//after cutoff
 
     //tolerance value
-    delta = 0.1
+    delta = 0.1;
 
     if (fabs(A1.x - (-285.34)) < delta)
 	{
@@ -34,20 +38,30 @@ int test_LMC_accel()
 	}
     if (fabs(A3.x - (-574.52)) < delta)
         {
-	    printf("cutoff Hernquist (before) test failed: accel=%f\n",A3)
+	    printf("cutoff Hernquist (before) test failed: accel=%f\n",A3);
             failed = 1;
 	}
     if (fabs(A4.x - (-1319.8)) < delta)
 	{
-	    printf("cutoff Hernquist (after) test failed: accel=%f\n",A4)
+	    printf("cutoff Hernquist (after) test failed: accel=%f\n",A4);
             failed = 1;
 	}
 
-    printf("\tPassed LMC accel test\n");
-    fflush(stdout);
-
     return failed;
 
+}
+
+/* Main function to run the test */
+int main()
+{   
+    int failed = test_LMC_accel();
+
+    if(failed == 0)
+    {
+        printf("All tests successful!\n");
+    }
+
+    return failed;
 }
 
 
