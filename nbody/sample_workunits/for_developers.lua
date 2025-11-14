02629fe66f31d1a1f9b2a99a4f6cdb2d4cf09215
Samin Alam <saminalam1124@gmail.com>
2025-11-14 03:28:12 -0500

    added bfe potential flag to lua
    more merging

--- nbody/sample_workunits/for_developers.lua
+++ nbody/sample_workunits/for_developers.lua
@@ -78,6 +78,8 @@ SunVely               = 229.2     -- -- Sun's y-velocity (kpc/Gyr)              
 SunVelz               = 6.9       -- -- Sun's z-velocity (kpc/Gyr)                                             -- --
 
 UseOldSofteningLength = 0         -- -- Uses old softening length formula from v1.76 and eariler               -- --
+
+Use_BFE_Potential     = false     -- -- Flag for using Basis Function Expansion for calculating potential      -- --
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 arg = { ... } -- -- TAKING USER INPUT
