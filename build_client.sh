f9b12a4cda251fbdee486d9b6754f1300a41513b
Kevnroux <rouxk@rpi.edu>
2026-04-09 11:43:10 -0400

    Fixing merge conflicts

--- build_client.sh
+++ build_client.sh
@@ -11,5 +11,4 @@ rm -r build
 mkdir build
 cd build
 cmake  -DCMAKE_BUILD_TYPE=Release -DNBODY_DEV_OPTIONS=ON -DNBODY_GL=OFF -DBOINC_APPLICATION=OFF -DSEPARATION=OFF -DDOUBLEPREC=ON -DNBODY_OPENMP=ON -DNBODY_OPENCL=OFF $PathToMilkyWayAtHomeClientDirectory/
-make clean
 make -j
